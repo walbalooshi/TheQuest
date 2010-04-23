9022f2f332c113fff2b2521a796c942b94dce389
Waleed Al-Balooshi <waleed.albaloushi@gmail.com>
2010-04-22 21:17:05 -0400

    design dungeon form and setup sprites

---
+++ TheQuest/Dungeon.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace TheQuest {
+    partial class Dungeon {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.PlayerSprite = new System.Windows.Forms.PictureBox();
+            this.SwordSprite = new System.Windows.Forms.PictureBox();
+            this.BatSprite = new System.Windows.Forms.PictureBox();
+            this.GhoulSprite = new System.Windows.Forms.PictureBox();
+            this.GhostSprite = new System.Windows.Forms.PictureBox();
+            this.BowSprite = new System.Windows.Forms.PictureBox();
+            this.RedPotionSprite = new System.Windows.Forms.PictureBox();
+            this.BluePotionSprite = new System.Windows.Forms.PictureBox();
+            this.MaceSprite = new System.Windows.Forms.PictureBox();
+            this.SwordInvSprite = new System.Windows.Forms.PictureBox();
+            this.RedPotionInvSprite = new System.Windows.Forms.PictureBox();
+            this.BluePotionInvSprite = new System.Windows.Forms.PictureBox();
+            this.BowInvSprite = new System.Windows.Forms.PictureBox();
+            this.MaceInvSprite = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SwordSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BatSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GhoulSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GhostSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BowSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPotionSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BluePotionSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaceSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SwordInvSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPotionInvSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BluePotionInvSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BowInvSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaceInvSprite)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayerSprite
+            // 
+            this.PlayerSprite.BackColor = System.Drawing.Color.Transparent;
+            this.PlayerSprite.Image = global::TheQuest.Properties.Resources.player;
+            this.PlayerSprite.Location = new System.Drawing.Point(76, 57);
+            this.PlayerSprite.Name = "PlayerSprite";
+            this.PlayerSprite.Size = new System.Drawing.Size(30, 30);
+            this.PlayerSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PlayerSprite.TabIndex = 0;
+            this.PlayerSprite.TabStop = false;
+            this.PlayerSprite.Visible = false;
+            // 
+            // SwordSprite
+            // 
+            this.SwordSprite.BackColor = System.Drawing.Color.Transparent;
+            this.SwordSprite.Image = global::TheQuest.Properties.Resources.sword;
+            this.SwordSprite.Location = new System.Drawing.Point(112, 57);
+            this.SwordSprite.Name = "SwordSprite";
+            this.SwordSprite.Size = new System.Drawing.Size(30, 30);
+            this.SwordSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SwordSprite.TabIndex = 1;
+            this.SwordSprite.TabStop = false;
+            this.SwordSprite.Visible = false;
+            // 
+            // BatSprite
+            // 
+            this.BatSprite.BackColor = System.Drawing.Color.Transparent;
+            this.BatSprite.Image = global::TheQuest.Properties.Resources.bat;
+            this.BatSprite.Location = new System.Drawing.Point(148, 57);
+            this.BatSprite.Name = "BatSprite";
+            this.BatSprite.Size = new System.Drawing.Size(30, 30);
+            this.BatSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BatSprite.TabIndex = 2;
+            this.BatSprite.TabStop = false;
+            this.BatSprite.Visible = false;
+            // 
+            // GhoulSprite
+            // 
+            this.GhoulSprite.BackColor = System.Drawing.Color.Transparent;
+            this.GhoulSprite.Image = global::TheQuest.Properties.Resources.ghoul;
+            this.GhoulSprite.Location = new System.Drawing.Point(184, 57);
+            this.GhoulSprite.Name = "GhoulSprite";
+            this.GhoulSprite.Size = new System.Drawing.Size(30, 30);
+            this.GhoulSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GhoulSprite.TabIndex = 3;
+            this.GhoulSprite.TabStop = false;
+            this.GhoulSprite.Visible = false;
+            // 
+            // GhostSprite
+            // 
+            this.GhostSprite.BackColor = System.Drawing.Color.Transparent;
+            this.GhostSprite.Image = global::TheQuest.Properties.Resources.ghost;
+            this.GhostSprite.Location = new System.Drawing.Point(220, 57);
+            this.GhostSprite.Name = "GhostSprite";
+            this.GhostSprite.Size = new System.Drawing.Size(30, 30);
+            this.GhostSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GhostSprite.TabIndex = 4;
+            this.GhostSprite.TabStop = false;
+            this.GhostSprite.Visible = false;
+            // 
+            // BowSprite
+            // 
+            this.BowSprite.BackColor = System.Drawing.Color.Transparent;
+            this.BowSprite.Image = global::TheQuest.Properties.Resources.bow;
+            this.BowSprite.Location = new System.Drawing.Point(256, 57);
+            this.BowSprite.Name = "BowSprite";
+            this.BowSprite.Size = new System.Drawing.Size(30, 30);
+            this.BowSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BowSprite.TabIndex = 5;
+            this.BowSprite.TabStop = false;
+            this.BowSprite.Visible = false;
+            // 
+            // RedPotionSprite
+            // 
+            this.RedPotionSprite.BackColor = System.Drawing.Color.Transparent;
+            this.RedPotionSprite.Image = global::TheQuest.Properties.Resources.potion_red;
+            this.RedPotionSprite.Location = new System.Drawing.Point(292, 57);
+            this.RedPotionSprite.Name = "RedPotionSprite";
+            this.RedPotionSprite.Size = new System.Drawing.Size(30, 30);
+            this.RedPotionSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RedPotionSprite.TabIndex = 6;
+            this.RedPotionSprite.TabStop = false;
+            this.RedPotionSprite.Visible = false;
+            // 
+            // BluePotionSprite
+            // 
+            this.BluePotionSprite.BackColor = System.Drawing.Color.Transparent;
+            this.BluePotionSprite.Image = global::TheQuest.Properties.Resources.potion_blue;
+            this.BluePotionSprite.Location = new System.Drawing.Point(328, 57);
+            this.BluePotionSprite.Name = "BluePotionSprite";
+            this.BluePotionSprite.Size = new System.Drawing.Size(30, 30);
+            this.BluePotionSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BluePotionSprite.TabIndex = 7;
+            this.BluePotionSprite.TabStop = false;
+            this.BluePotionSprite.Visible = false;
+            // 
+            // MaceSprite
+            // 
+            this.MaceSprite.BackColor = System.Drawing.Color.Transparent;
+            this.MaceSprite.Image = global::TheQuest.Properties.Resources.mace;
+            this.MaceSprite.Location = new System.Drawing.Point(364, 57);
+            this.MaceSprite.Name = "MaceSprite";
+            this.MaceSprite.Size = new System.Drawing.Size(30, 30);
+            this.MaceSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MaceSprite.TabIndex = 8;
+            this.MaceSprite.TabStop = false;
+            this.MaceSprite.Visible = false;
+            // 
+            // SwordInvSprite
+            // 
+            this.SwordInvSprite.BackColor = System.Drawing.Color.Transparent;
+            this.SwordInvSprite.Image = global::TheQuest.Properties.Resources.sword;
+            this.SwordInvSprite.Location = new System.Drawing.Point(94, 321);
+            this.SwordInvSprite.Name = "SwordInvSprite";
+            this.SwordInvSprite.Size = new System.Drawing.Size(50, 50);
+            this.SwordInvSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SwordInvSprite.TabIndex = 9;
+            this.SwordInvSprite.TabStop = false;
+            this.SwordInvSprite.Visible = false;
+            // 
+            // RedPotionInvSprite
+            // 
+            this.RedPotionInvSprite.BackColor = System.Drawing.Color.Transparent;
+            this.RedPotionInvSprite.Image = global::TheQuest.Properties.Resources.potion_red;
+            this.RedPotionInvSprite.Location = new System.Drawing.Point(156, 321);
+            this.RedPotionInvSprite.Name = "RedPotionInvSprite";
+            this.RedPotionInvSprite.Size = new System.Drawing.Size(50, 50);
+            this.RedPotionInvSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RedPotionInvSprite.TabIndex = 10;
+            this.RedPotionInvSprite.TabStop = false;
+            this.RedPotionInvSprite.Visible = false;
+            // 
+            // BluePotionInvSprite
+            // 
+            this.BluePotionInvSprite.BackColor = System.Drawing.Color.Transparent;
+            this.BluePotionInvSprite.Image = global::TheQuest.Properties.Resources.potion_blue;
+            this.BluePotionInvSprite.Location = new System.Drawing.Point(280, 321);
+            this.BluePotionInvSprite.Name = "BluePotionInvSprite";
+            this.BluePotionInvSprite.Size = new System.Drawing.Size(50, 50);
+            this.BluePotionInvSprite.TabIndex = 11;
+            this.BluePotionInvSprite.TabStop = false;
+            this.BluePotionInvSprite.Visible = false;
+            // 
+            // BowInvSprite
+            // 
+            this.BowInvSprite.BackColor = System.Drawing.Color.Transparent;
+            this.BowInvSprite.Image = global::TheQuest.Properties.Resources.bow;
+            this.BowInvSprite.Location = new System.Drawing.Point(218, 321);
+            this.BowInvSprite.Name = "BowInvSprite";
+            this.BowInvSprite.Size = new System.Drawing.Size(50, 50);
+            this.BowInvSprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BowInvSprite.TabIndex = 12;
+            this.BowInvSprite.TabStop = false;
+            this.BowInvSprite.Visible = false;
+            // 
+            // MaceInvSprite
+            // 
+            this.MaceInvSprite.BackColor = System.Drawing.Color.Transparent;
+            this.MaceInvSprite.Image = global::TheQuest.Properties.Resources.mace;
+            this.MaceInvSprite.Location = new System.Drawing.Point(342, 321);
+            this.MaceInvSprite.Name = "MaceInvSprite";
+            this.MaceInvSprite.Size = new System.Drawing.Size(50, 50);
+            this.MaceInvSprite.TabIndex = 13;
+            this.MaceInvSprite.TabStop = false;
+            this.MaceInvSprite.Visible = false;
+            // 
+            // Dungeon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TheQuest.Properties.Resources.dungeon600x400;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(752, 399);
+            this.Controls.Add(this.MaceInvSprite);
+            this.Controls.Add(this.BowInvSprite);
+            this.Controls.Add(this.BluePotionInvSprite);
+            this.Controls.Add(this.RedPotionInvSprite);
+            this.Controls.Add(this.SwordInvSprite);
+            this.Controls.Add(this.PlayerSprite);
+            this.Controls.Add(this.GhostSprite);
+            this.Controls.Add(this.GhoulSprite);
+            this.Controls.Add(this.BatSprite);
+            this.Controls.Add(this.MaceSprite);
+            this.Controls.Add(this.BluePotionSprite);
+            this.Controls.Add(this.RedPotionSprite);
+            this.Controls.Add(this.BowSprite);
+            this.Controls.Add(this.SwordSprite);
+            this.Name = "Dungeon";
+            this.Text = "The Quest";
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SwordSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BatSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GhoulSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GhostSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BowSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPotionSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BluePotionSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaceSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SwordInvSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPotionInvSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BluePotionInvSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BowInvSprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaceInvSprite)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PlayerSprite;
+        private System.Windows.Forms.PictureBox SwordSprite;
+        private System.Windows.Forms.PictureBox BatSprite;
+        private System.Windows.Forms.PictureBox GhoulSprite;
+        private System.Windows.Forms.PictureBox GhostSprite;
+        private System.Windows.Forms.PictureBox BowSprite;
+        private System.Windows.Forms.PictureBox RedPotionSprite;
+        private System.Windows.Forms.PictureBox BluePotionSprite;
+        private System.Windows.Forms.PictureBox MaceSprite;
+        private System.Windows.Forms.PictureBox SwordInvSprite;
+        private System.Windows.Forms.PictureBox RedPotionInvSprite;
+        private System.Windows.Forms.PictureBox BluePotionInvSprite;
+        private System.Windows.Forms.PictureBox BowInvSprite;
+        private System.Windows.Forms.PictureBox MaceInvSprite;
+    }
+}
+
