d86619b54f7a434e99062e6835caa3f115d67e03
Waleed Al-Balooshi <waleed.albaloushi@gmail.com>
2010-05-10 18:29:29 -0400

    Added Ghost class

---
+++ TheQuest/Ghost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace TheQuest {
+    public class Ghost : Enemy, ISprite {
+        public Ghost(Game game, Point location, Size spriteSize) 
+            : base(game, location, 8, spriteSize) { }
+
+        public override void Move(Random random) {
+            if (random.Next(1, 3) == 1) {
+                location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);
+            }
+
+            if (NearPlayer()) {
+                game.HitPlayer(3, random);
+            }
+        }
+    }
+}
